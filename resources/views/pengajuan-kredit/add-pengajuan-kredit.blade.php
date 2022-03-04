6ff051e6c5bb23b079d495520a3d4675a6fac450
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2022-03-04 16:43:02 +0700

    udpate add js pengajuan kredit

--- resources/views/pengajuan-kredit/add-pengajuan-kredit.blade.php
+++ resources/views/pengajuan-kredit/add-pengajuan-kredit.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.template')
 @section('content')
+<div id="form-data-umum" class="form-wizard active" data-index='0' data-done='false'>
     <div class="row form-group">
         <div class="col-md-6">
             <label for="">Label 1</label>
@@ -40,10 +41,109 @@
             <textarea name="" class="form-control" id="" rows="5"></textarea>
         </div>
     </div>
+</div>
+<div id="form-2" class="form-wizard" data-index='1' data-done="false">
     <div class="row form-group">
-        <div class="col text-right">
-            <button class="btn btn-default"><span class="fa fa-chevron-left"></span> Sebelumnya</button>
-            <button class="btn btn-danger">Selanjutnya <span class="fa fa-chevron-right"></span></button>
+        <div class="col-md-6">
+            <label for="">Label 5</label>
+            <select name="" id="" class="form-control select2">
+                <option value="">---Pilih Select---</option>
+            </select>
+        </div>
+        <div class="col-md-6">
+            <label for="">Label 6</label>
+            <input type="text" class="form-control" placeholder="Input label 6">
         </div>
     </div>
+    <div class="row form-group">
+        <div class="col-md-6">
+            <label for="">Label 5</label>
+            <select name="" id="" class="form-control select2">
+                <option value="">---Pilih Select---</option>
+            </select>
+        </div>
+        <div class="col-md-6">
+            <label for="">Label 6</label>
+            <input type="text" class="form-control" placeholder="Input label 6">
+        </div>
+    </div>
+</div>
+<div id="form-3" class="form-wizard" data-index='2' data-done="false">
+    <div class="row form-group">
+        <div class="col-md-6">
+            <label for="">Label 5</label>
+            <select name="" id="" class="form-control select2">
+                <option value="">---Pilih Select---</option>
+            </select>
+        </div>
+        <div class="col-md-6">
+            <label for="">Label 6</label>
+            <input type="text" class="form-control" placeholder="Input label 6">
+        </div>
+    </div>
+</div>
+<div class="row form-group">
+    <div class="col text-right">
+        <button class="btn btn-default btn-prev"><span class="fa fa-chevron-left"></span> Sebelumnya</button>
+        <button class="btn btn-danger btn-next">Selanjutnya <span class="fa fa-chevron-right"></span></button>
+    </div>
+</div>
 @endsection
+
+@push('custom-script')
+<script>
+
+    function cekBtn(){
+        var indexNow = $(".form-wizard.active").data('index')
+        var prev = parseInt(indexNow) - 1
+        var next = parseInt(indexNow) + 1
+
+        $(".btn-prev").hide()
+
+        if($(".form-wizard[data-index='"+prev+"']").length==1){
+            $(".btn-prev").show()
+        }
+    }
+    function cekWizard(isNext=false){
+        var indexNow = $(".form-wizard.active").data('index')
+        if(isNext){
+            $(".side-wizard li").removeClass('active')
+        }
+        
+        $(".side-wizard li").removeClass('selected')
+        
+        for (let index = 0; index <= parseInt(indexNow); index++) {
+            var selected = index==parseInt(indexNow) ? ' selected' : ''
+            $(".side-wizard li[data-index='"+index+"']").addClass('active'+selected)
+            $(".side-wizard li[data-index='"+index+"'] a span i").removeClass('fa-ban')
+            $(".side-wizard li[data-index='"+index+"'] a span i").addClass('fa-check')
+        }
+    }
+    cekBtn()
+    cekWizard()
+
+    $(".btn-next").click(function(){
+        var indexNow = $(".form-wizard.active").data('index')
+        var next = parseInt(indexNow) + 1
+
+        if($(".form-wizard[data-index='"+next+"']").length==1){
+            $(".form-wizard").removeClass('active')
+            $(".form-wizard[data-index='"+next+"']").addClass('active')
+            $(".form-wizard[data-index='"+indexNow+"']").attr('data-done','true')
+        }
+
+        cekWizard()
+        cekBtn(true)
+    })
+    $(".btn-prev").click(function(){
+        var indexNow = $(".form-wizard.active").data('index')
+        var prev = parseInt(indexNow) - 1
+        if($(".form-wizard[data-index='"+prev+"']").length==1){
+            $(".form-wizard").removeClass('active')
+            $(".form-wizard[data-index='"+prev+"']").addClass('active')
+        }
+        cekWizard()
+        cekBtn()        
+    })
+</script>
+@endpush
