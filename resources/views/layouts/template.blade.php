5ef84e6167a36fc6c02b12a8ce5a6276cd5f434e
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2022-03-22 08:23:19 +0700

    Update template.blade.php

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -68,7 +68,7 @@
             <ul class="navbar-nav">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <span class="fa fa-user"></span> Admin
+                      <span class="fa fa-user"></span> {{auth()->user()->name}}
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                       <a class="dropdown-item" href="{{ route('change_password') }}">Ganti Password</a>
