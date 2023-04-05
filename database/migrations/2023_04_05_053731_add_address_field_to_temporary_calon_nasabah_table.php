cbf49d694b720548a146cc0fa61c3e14eaa39fca
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-04-05 09:55:34 +0700

    chore(migration): add sektor_usaha to temp nasabah table

--- database/migrations/2023_04_05_053731_add_address_field_to_temporary_calon_nasabah_table.php
+++ database/migrations/2023_04_05_053731_add_address_field_to_temporary_calon_nasabah_table.php
@@ -14,6 +14,7 @@ class AddAddressFieldToTemporaryCalonNasabahTable extends Migration
     public function up()
     {
         Schema::table('temporary_calon_nasabah', function (Blueprint $table) {
+            $table->string('sektor_kredit', 255)->nullable()->after('status');
             $table->bigInteger('id_kabupaten')->nullable()->after('id_user');
             $table->bigInteger('id_kecamatan')->nullable()->after('id_kabupaten');
             $table->bigInteger('id_desa')->nullable()->after('id_kecamatan');
@@ -29,6 +30,7 @@ class AddAddressFieldToTemporaryCalonNasabahTable extends Migration
     {
         Schema::table('temporary_calon_nasabah', function (Blueprint $table) {
             $table->dropColumn([
+                'sektor_kredit',
                 'id_kabupaten',
                 'id_kecamatan',
                 'id_desa',
