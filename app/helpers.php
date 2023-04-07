44455e55cd87b24f6d37ca19174902bbefb0f56e
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-04-07 15:12:47 +0700

    feat: show saved filename

---
+++ app/helpers.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Models\JawabanTemp;
+
+if(!function_exists('temporary')) {
+    function temporary(int $id) {
+        return JawabanTemp::where('id_jawaban', $id)
+            ->first()?->opsi_text;
+    }
+}
