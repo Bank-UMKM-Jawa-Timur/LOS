762b5d1a7aa8b47a6b568b0276af671adb28a3d0
Hafidz Fadhillah Febrianto <63035660+Hafidz1121@users.noreply.github.com>
2023-05-07 00:47:32 +0700

    Revert "Add Popup Upload File And Add Form Data Po To Data Umum"
    This reverts commit 624ecb2975c9170450e2be6bd976b1a20cb38885.

--- resources/views/pengajuan-kredit/komentar-pincab-pengajuan.blade.php
+++ resources/views/pengajuan-kredit/komentar-pincab-pengajuan.blade.php
@@ -211,11 +211,11 @@
                                         <a href="{{ route('pengajuan.check.pincab.status.detail',$item->id_pengajuan) }}" class="dropdown-item">Review</a>
                                         <a href="#" class="dropdown-item" data-toggle="modal" data-id="{{ $item->id_pengajuan }}" data-target="#exampleModal-{{$item->id_pengajuan}}">Disetujui / Ditolak</a>
                                         <a target="_blank" href="{{ route('cetak',$item->id_pengajuan) }}" class="dropdown-item" >Cetak</a>
-                                        <a href="#" class="dropdown-item" data-toggle="modal" data-id="{{ $item->id_pengajuan }}" data-target="#uploadModal-{{$item->id_pengajuan}}">Upload File</a>
                                         @if ($item->skema_kredit == 'KKB')
                                             <a target="_blank" href="{{ route('cetak-sppk',$item->id_pengajuan)}}" class="dropdown-item" >Cetak SPPK</a>
                                             <a target="_blank" href="{{ route('cetak-pk',$item->id_pengajuan)}}" class="dropdown-item" >Cetak PK</a>
                                             <a target="_blank" href="{{ route('cetak-po',$item->id_pengajuan)}}" class="dropdown-item" >Cetak PO</a>
+                                            <a href="{{ route('upload-file', $item->id_pengajuan) }}" class="dropdown-item">Upload File</a>
                                         @endif
                                     </div>
                                 </div>
@@ -231,7 +231,7 @@
                                         <a target="_blank" href="{{ route('cetak-sppk',$item->id_pengajuan)}}" class="dropdown-item" >Cetak SPPK</a>
                                         <a target="_blank" href="{{ route('cetak-pk',$item->id_pengajuan)}}" class="dropdown-item" >Cetak PK</a>
                                         <a target="_blank" href="{{ route('cetak-po',$item->id_pengajuan)}}" class="dropdown-item" >Cetak PO</a>
-                                        <a href="#" class="dropdown-item" data-toggle="modal" data-id="{{ $item->id_pengajuan }}" data-target="#uploadModal-{{$item->id_pengajuan}}">Upload File</a>
+                                        <a href="{{ route('upload-file', $item->id_pengajuan) }}" class="dropdown-item">Upload File</a>
                                     @endif
                                 </div>
                             </div>
@@ -266,5 +266,4 @@
     </div>
 </div>
 @include('layouts.modal')
-@include('layouts.popup-upload')
 @endsection
