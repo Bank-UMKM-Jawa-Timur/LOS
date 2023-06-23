96dcea582bafbff320dc993edf487cbddeac1de9
oong <37205723+oong26@users.noreply.github.com>
2023-06-23 09:45:14 +0700

    fix dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,7 +20,7 @@ class DashboardController extends Controller
             $param['data'] = PengajuanModel::select('pengajuan.id', 'pengajuan.id_staf', 'pengajuan.tanggal', 'pengajuan.posisi', 'pengajuan.progress_pengajuan_data', 'pengajuan.tanggal_review_penyelia', 'pengajuan.tanggal_review_pincab', 'pengajuan.status', 'pengajuan.status_by_sistem', 'pengajuan.id_cabang', 'pengajuan.average_by_sistem', 'pengajuan.average_by_penyelia', 'calon_nasabah.nama', 'calon_nasabah.jenis_usaha', 'calon_nasabah.sektor_kredit', 'calon_nasabah.jumlah_kredit', 'calon_nasabah.id_pengajuan')
                 ->join('calon_nasabah', 'calon_nasabah.id_pengajuan', 'pengajuan.id')
                 ->where('pengajuan.id_cabang', $id_cabang)
-                ->where('pengajuan.posisi', '=', 'Proses Input Data')
+                ->where('pengajuan.posisi', 'Proses Input Data')
                 ->where('pengajuan.id_staf', $id_data)
                 ->get();
             // dd($param['data']);
