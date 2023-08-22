bb25ce0ec763886677eb9efcd9aae839d3065c4d
oong26 <mkhalil26122000@gmail.com>
2023-08-22 16:38:24 +0700

    develop : create reset session confirm modal

---
+++ resources/views/user/sessions/confirm-modal.blade.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="confirmResetSession" tabindex="-1" role="dialog" aria-labelledby="confirmResetSessionLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmModalLabel">Konfirmasi</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+            </div>
+            <div class="modal-body">
+                <p>Apakah Anda yakin akan mereset sesi ini?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                <button type="submit" id="btn-hapus" class="btn btn-danger">Reset</button>
+            </div>
+        </div>
+    </div>
+</div>
