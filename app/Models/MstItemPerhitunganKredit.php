bd14eee621815e2793b77c55bee20dafe1f87c2c
oong26 <mkhalil26122000@gmail.com>
2023-08-28 16:22:14 +0700

    enc/aspek-keuangan : create model & migration

---
+++ app/Models/MstItemPerhitunganKredit.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MstItemPerhitunganKredit extends Model
+{
+    use HasFactory;
+    protected $table = 'mst_item_perhitungan_kredit';
+}
