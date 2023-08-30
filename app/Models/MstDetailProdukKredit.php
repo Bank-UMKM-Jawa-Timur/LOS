0ad647032251e2f9c1f23ffa28054734d75e9dd8
oong26 <mkhalil26122000@gmail.com>
2023-08-30 10:33:07 +0700

    enc/aspek-keuangan : create MstDetailProdukKredit model & seeder

---
+++ app/Models/MstDetailProdukKredit.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MstDetailProdukKredit extends Model
+{
+    use HasFactory;
+    protected $table = 'mst_detail_produk_kredit';
+}
