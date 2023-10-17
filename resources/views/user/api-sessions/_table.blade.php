a3d3bb181018410fa29d091491e4790f381f6fbd
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2023-10-17 20:32:35 +0700

    main: fixing waktu di session & api session

--- resources/views/user/api-sessions/_table.blade.php
+++ resources/views/user/api-sessions/_table.blade.php
@@ -19,7 +19,7 @@
                 $no = !$page || $page == 1 ? 1 : ($page - 1) * 10 + 1;
             @endphp
             @foreach ($data as $key => $item)
-                @php
+                {{-- @php
                     $cabang = '-';
                     if ($item->id_cabang) {
                         $dataCabang = DB::table('cabang')
@@ -38,6 +38,29 @@
                     // formating seconds
                     $secs = explode('.', number_format((float)(($end - $start) / 60), 2))[1];
                     $secs = $secs > 60 ? 0 : $secs;
+                @endphp --}}
+                @php
+                    $cabang = '-';
+                    if ($item->id_cabang) {
+                        $dataCabang = DB::table('cabang')
+                            ->where('id', $item->id_cabang)
+                            ->first();
+                        $cabang = $dataCabang->cabang;
+                    }
+                
+                    // Waktu login pengguna
+                    $startTime = new DateTime($item->created_at);
+                
+                    // Waktu saat ini
+                    $endTime = new DateTime('now');
+                
+                    // Hitung perbedaan waktu
+                    $interval = $endTime->diff($startTime);
+                
+                    // Format waktu
+                    $hours = $interval->h;
+                    $minutes = $interval->i;
+                    $seconds = $interval->s;
                 @endphp
                 <tr class="border-bottom-primary">
                     <td class="text-center text-muted">{{ $no }}</td>
@@ -55,29 +78,29 @@
                     <td>
                         <span class="clock_{{$item->id}}"></span>
                         <script>
-                            currentTime({{$hours}}, {{$mins}}, {{$secs}}, "clock_{{$item->id}}")
+                            currentTime({{$hours}}, {{$minutes}}, {{$seconds}}, "clock_{{$item->id}}")
                             function currentTime(h, m, s, widget_id) {
                                 let hh = parseInt(h);
                                 let mm = parseInt(m);
                                 let ss = parseInt(s);
                                 ss++;
-
+                    
                                 if (ss > 59) {
                                     mm++;
                                     ss = 0;
                                 }
-
+                    
                                 if (mm > 59) {
                                     hh++;
-                                    mm = 00;
+                                    mm = 0;
                                 }
-
+                    
                                 hh = (hh < 10) ? "0" + hh : hh;
                                 mm = (mm < 10) ? "0" + mm : mm;
                                 ss = (ss < 10) ? "0" + ss : ss;
-                                
+                    
                                 let time = hh + ":" + mm + ":" + ss;
-                                $(`.${widget_id}`).html(time)
+                                document.querySelector(`.${widget_id}`).innerHTML = time;
                                 var t = setTimeout(function(){ currentTime(hh, mm, ss, `${widget_id}`) }, 1000); 
                             }
                         </script>
