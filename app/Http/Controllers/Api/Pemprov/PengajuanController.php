bc9a661c724cbd9f9741df3bfff0c5dba9b9fb76
LuciferLovesMe <abhiemsmile@gmail.com>
2023-12-10 13:08:55 +0700

    api-dagulir: Store pengajuan

--- app/Http/Controllers/Api/Pemprov/PengajuanController.php
+++ app/Http/Controllers/Api/Pemprov/PengajuanController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers\Api\Pemprov;
 
 use App\Http\Controllers\Controller;
+use App\Models\PengajuanDagulir;
+use App\Models\PengajuanModel;
+use App\Models\User;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class PengajuanController extends Controller
 {
@@ -16,50 +22,146 @@ class PengajuanController extends Controller
          */
         $status = '';
         $message = '';
+        $req_status = Response::HTTP_OK;
 
         DB::beginTransaction();
         try {
-            // 1. Insert to pengajuan table
-            $pengajuan = [
-                'tanggal' => $request->get('tanggal'),
-                'id_cabang' => $request->get('id_cabang'),
-                'skema_kredit' => $request->get('skema_kredit'),
-            ];
+            $req = $request->all();
+            $validator = Validator::make($req, [
+                'kode_pendaftaran' => 'required|unique:pengajuan_dagulir,kode_pendaftaran',
+                'nama' => 'required',
+                'nik' => 'required',
+                'tempat_lahir' => 'required',
+                'tanggal_lahir' => 'required',
+                'telp' => 'required',
+                'jenis_usaha' => 'required',
+                'nominal' => 'required',
+                'tujuan_penggunaan' => 'required',
+                'jangka_waktu' => 'required',
+                'kode_bank_pusat' => 'required',
+                'kode_bank_cabang' => 'required',
+                'kec_ktp' => 'required',
+                'kotakab_ktp' => 'required',
+                'alamat_ktp' => 'required',
+                'kec_dom' => 'required',
+                'kotakab_dom' => 'required',
+                'alamat_dom' => 'required',
+                'kec_usaha' => 'required',
+                'kotakab_usaha' => 'required',
+                'alamat_usaha' => 'required',
+                'tipe' => 'required',
+                'npwp' => 'required',
+                'jenis_badan_hukum' => 'required',
+                'tempat_berdiri' => 'required',
+                'tanggal_berdiri' => 'required',
+                'tanggal' => 'required',
+            ], [
+                'required' => ':attribute harus diisi.',
+                'unique' => ':attribute telah digunakan'
+            ], [
+                'kode_pendaftaran' => 'Kode Pendaftaran',
+                'nama' => 'Nama',
+                'nik' => 'NIK',
+                'tempat_lahir' => 'Tempat Lahir',
+                'tanggal_lahir' => 'Tanggal Lahir',
+                'telp' => 'Telp',
+                'jenis_usaha' => 'Jenis Usaha',
+                'nominal' => 'Nominal',
+                'tujuan_penggunaan' => 'Tujuan Penggunaan',
+                'jangka_waktu' => 'Jangka Waktu',
+                'kode_bank_pusat' => 'Kode Bank Pusat',
+                'kode_bank_cabang' => 'Kode Bank Cabang',
+                'kec_ktp' => 'Kecamatan KTP',
+                'kotakab_ktp' => 'Kota/Kabupaten KTP',
+                'alamat_ktp' => 'Alamat KTP',
+                'kec_dom' => 'Kecamatan Domisili',
+                'kotakab_dom' => 'Kota/Kabupaten Domisili',
+                'alamat_dom' => 'Alatmat Domisili',
+                'kec_usaha' => 'Kecamatan Usaha',
+                'kotakab_usaha' => 'Kota/Kabupaten Usaha',
+                'alamat_usaha' => 'Alamat Usaha',
+                'tipe' => 'Tipe',
+                'npwp' => 'NPWP',
+                'jenis_badan_hukum' => 'Jenis Badan Hukum',
+                'tempat_berdiri' => 'Tempat Berdiri',
+                'tanggal_berdiri' => 'Tanggal Berdiri',
+                'tanggal' => 'Tanggal',
+            ]);
 
-            $idPengajuan = DB::table('pengajuan')->insertGetId($pengajuan);
-
-            // 2. Insert to pengajuan table
-            $nasabah = [
-                'nama' => $request->get('nama_nasabah'),
-                'alamat_rumah' => $request->get('alamat_rumah'),
-                'alamat_usaha' => $request->get('alamat_usaha'),
-                'no_ktp' => $request->get('no_ktp'),
-                'tempat_lahir' => $request->get('tempat_lahir'),
-                'tanggal_lahir' => date('Y-m-d', strtotime($request->get('tanggal_lahir'))),
-                'status' => $request->get('status_menikah'),
-                'sektor_kredit' => $request->get('sektor_kredit'),
-                'jenis_usaha' => $request->get('jenis_usaha'),
-                'jumlah_kredit' => $request->get('nominal'),
-                'tenor_yang_diminta' => $request->get('tenor'),
-                'tujuan_kredit' => $request->get('tujuan_kredit'),
-                'jaminan_kredit' => $request->get('jaminan'),
-                'hubungan_bank' => $request->get('hubungan_bank'),
-                'verifikasi_umum' => $request->get('hasil_verifikasi'),
-                'id_user' => 1,
-                'id_desa' => 3505012001,
-                'id_kecamatan' => 350501,
-                'id_kabupaten' => 3505,
-            ];
-            DB::table('calon_nasabah')->insert($nasabah);
-
-            $status = 'success';
-            $message = 'Successfully store data';
-
-            DB::commit();
+            if($validator->fails()){
+                $req_status = Response::HTTP_UNPROCESSABLE_ENTITY;
+                $message = $validator->errors()->all();
+                $status = 'failed';
+            } else{
+                $userId = User::where('user_dagulir', 1)
+                    ->where('id_cabang', $request->get('kode_bank_cabang'))
+                    ->first();
+                if($userId == null){
+                    $req_status = Response::HTTP_UNPROCESSABLE_ENTITY;
+                    $message = 'User tidak dapat ditemukan pada bank cabang dengan kode '. $request->get('kode_bank_cabang');
+                    $status = 'failed';
+                } else{
+                    $insertDagulir = [
+                        'kode_pendaftaran' => $request->get('kode_pendaftaran'),
+                        'nama' => $request->get('nama'),
+                        'nik' => $request->get('nik'),
+                        'nama_pj_ketua' => $request->get('nama_pj_ketua') ?? null,
+                        'tempat_lahir' => $request->get('tempat_lahir'),
+                        'tanggal_lahir' => $request->get('tanggal_lahir'),
+                        'telp' => $request->get('telp'),
+                        'jenis_usaha' => $request->get('jenis_usaha'),
+                        'nominal' => $request->get('nominal'),
+                        'tujuan_penggunaan' => $request->get('tujuan_penggunaan'),
+                        'jangka_waktu' => $request->get('jangka_waktu'),
+                        'kode_bank_pusat' => $request->get('kode_bank_pusat'),
+                        'kode_bank_cabang' => $request->get('kode_bank_cabang'),
+                        'kec_ktp' => $request->get('kec_ktp'),
+                        'kotakab_ktp' => $request->get('kotakab_ktp'),
+                        'alamat_ktp' => $request->get('alamat_ktp'),
+                        'kec_dom' => $request->get('kec_dom'),
+                        'kotakab_dom' => $request->get('kotakab_dom'),
+                        'alamat_dom' => $request->get('alamat_dom'),
+                        'kec_usaha' => $request->get('kec_usaha'),
+                        'kotakab_usaha' => $request->get('kotakab_usaha'),
+                        'alamat_usaha' => $request->get('alamat_usaha'),
+                        'tipe' => $request->get('tipe'),
+                        'npwp' => $request->get('npwp'),
+                        'jenis_badan_hukum' => $request->get('jenis_badan_hukum'),
+                        'tempat_berdiri' => $request->get('tempat_berdiri'),
+                        'tanggal_berdiri' => $request->get('tanggal_berdiri'),
+                        'tanggal' => $request->get('tanggal'),
+                        'user_id' => $userId->id,
+                        'created_at' => now(),
+                    ];
+    
+                    $pengajuanDagulirId = PengajuanDagulir::insertGetId($insertDagulir);
+                    $dagulirId = DB::table('mst_produk_kredit')
+                        ->where('name', 'like', "%dagulir%")
+                        ->first();
+                    $addPengajuan = new PengajuanModel();
+                    $addPengajuan->id_staf = $userId->id;
+                    $addPengajuan->tanggal = date(now());
+                    $addPengajuan->id_cabang = $request->get('kode_bank_cabang');
+                    $addPengajuan->skema_kredit = 'Dagulir';
+                    $addPengajuan->produk_kredit_id = $dagulirId->id;
+                    $addPengajuan->dagulir_id = $pengajuanDagulirId;
+                    $addPengajuan->save();
+    
+                    DB::commit();
+                    $status = 'success';
+                    $message = 'Berhasil menambahkan data pengajuan dagulir.';
+                }
+            }
         } catch (\Exception $e) {
             DB::rollback();
             $status = 'failed';
             $message = $e->getMessage();
+            $req_status = Response::HTTP_BAD_REQUEST;
+        } catch(QueryException $e){
+            DB::rollBack();
+            $status = 'failed';
+            $message = $e->getMessage();
+            $req_status = Response::HTTP_BAD_REQUEST;
         } finally {
             // 3. Return json
             $response = [
@@ -67,7 +169,7 @@ class PengajuanController extends Controller
                 'message' => $message,
             ];
 
-            return response()->json($response);
+            return response()->json($response, $req_status);
         }
     }
 }
