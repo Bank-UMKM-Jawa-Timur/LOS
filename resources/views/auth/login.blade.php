89eafe3e838d7739d22bf90359a8f112884e2731
sahrullahh <mohammadsahrullah894@gmail.com>
2023-12-14 10:32:22 +0700

    feat/dagulir: improvment login ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
     >
       <div class="overlay bg-theme-secondary/90 w-full h-screen">
         <br />
-        <div class="container max-w-2xl mx-auto mt-20 lg:p-0 p-10">
+        <div class="container max-w-2xl mx-auto mt-[5vh] lg:p-0 p-10">
           <form
             method="post" id="login" action="{{ route('login') }}"
           >
@@ -52,19 +52,23 @@
                   >
                     Login Pincetar
                     @if (session('error'))
+                    <div class="p-2">
                         <div class="alert alert-danger">
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <i class="icofont icofont-close-line-circled text-white"></i>
                             </button>
                             <strong><span class="fa fa-exclamation-triangle"></span> Gagal login.</strong> {{session('error')}}
                         </div>
+                      </div>
                     @endif
                     @if ($errors->any())
+                    <div class="p-2">
                     <div class="alert alert-danger">
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <i class="icofont icofont-close-line-circled text-white"></i>
                         </button>
                         <strong><span class="fa fa-exclamation-triangle"></span> Gagal login.</strong> {{$errors}}
+                    </div>
                     </div>
                     @endif
                     <div class="mt-5 space-y-5 text-left">
@@ -76,7 +80,7 @@
                           Email atau NIP
                         </label>
                         <div
-                          class="form-input focus:ring-0 ring-offset-transparent border-2 rounded-md bg-gray-50 focus-within:bg-gray-100 flex"
+                          class=" focus:ring-0 ring-offset-transparent border-2 rounded-md bg-gray-50 focus-within:bg-gray-100 flex"
                         >
                           <!-- icon -->
                           <div class="text-center ml-0 mt-0 p-2 text-gray-400">
@@ -93,11 +97,13 @@
                           </div>
                           <input
                             type="text"
+                            id="email"
                             name="email"
                             value="{{ old('email') }}"
                             autocomplete="off"
-                            class="w-full p-2 bg-gray-50 font-medium rounded-md focus:bg-gray-100 outline-none"
+                            class="w-full p-2 bg-gray-50 font-medium text-sm rounded-md focus:bg-gray-100 outline-none"
                           />
+                          <small class="alert-message text-red-500 hidden">Isi email atau NIP terlebih dahulu.</small>
                         </div>
                       </div>
                       <div class="input-box space-y-4">
@@ -108,7 +114,7 @@
                           Password
                         </label>
                         <div
-                          class="form-input focus:ring-0 ring-offset-transparent  border-2 rounded-md bg-gray-50 focus-within:bg-gray-100 flex gap-2"
+                          class="focus:ring-0 ring-offset-transparent  border-2 rounded-md bg-gray-50 focus-within:bg-gray-100 flex gap-2"
                         >
                           <!-- icon -->
                           <div class="text-center ml-0 mt-0 p-2 text-gray-400">
@@ -127,26 +133,17 @@
                             type="password"
                             autocomplete="off"
                             name="password"
+                            id="password"
                             value="{{ old('password') }}"
-                            class="w-full p-2 bg-gray-50 rounded-md font-medium focus:bg-gray-100 outline-none"
+                            class="w-full p-2 bg-gray-50 text-sm rounded-md font-medium focus:bg-gray-100 outline-none"
                           />
-                          <div class="text-center mr-2 mt-0 p-2 text-gray-400">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              class="w-8"
-                              viewBox="0 0 256 256"
-                            >
-                              <path
-                                fill="currentColor"
-                                d="M53.92 34.62a8 8 0 1 0-11.84 10.76l19.24 21.17C25 88.84 9.38 123.2 8.69 124.76a8 8 0 0 0 0 6.5c.35.79 8.82 19.57 27.65 38.4C61.43 194.74 93.12 208 128 208a127.11 127.11 0 0 0 52.07-10.83l22 24.21a8 8 0 1 0 11.84-10.76Zm47.33 75.84l41.67 45.85a32 32 0 0 1-41.67-45.85ZM128 192c-30.78 0-57.67-11.19-79.93-33.25A133.16 133.16 0 0 1 25 128c4.69-8.79 19.66-33.39 47.35-49.38l18 19.75a48 48 0 0 0 63.66 70l14.73 16.2A112 112 0 0 1 128 192Zm6-95.43a8 8 0 0 1 3-15.72a48.16 48.16 0 0 1 38.77 42.64a8 8 0 0 1-7.22 8.71a6.39 6.39 0 0 1-.75 0a8 8 0 0 1-8-7.26A32.09 32.09 0 0 0 134 96.57Zm113.28 34.69c-.42.94-10.55 23.37-33.36 43.8a8 8 0 1 1-10.67-11.92a132.77 132.77 0 0 0 27.8-35.14a133.15 133.15 0 0 0-23.12-30.77C185.67 75.19 158.78 64 128 64a118.37 118.37 0 0 0-19.36 1.57A8 8 0 1 1 106 49.79A134 134 0 0 1 128 48c34.88 0 66.57 13.26 91.66 38.35c18.83 18.83 27.3 37.62 27.65 38.41a8 8 0 0 1 0 6.5Z"
-                              />
-                            </svg>
-                          </div>
+                          <small class="alert-message text-red-500 hidden">Isi password terlebih dahulu.</small>
                         </div>
                       </div>
                       <button
                         type="submit"
-                        class="bg-theme-primary py-5 rounded-md font-semibold tracking-tighter text-white w-full"
+                        id="login-button"
+                        class="bg-theme-primary hover:bg-red-700 py-5 rounded-md font-semibold tracking-tighter text-white w-full"
                       >
                         Masuk
                       </button>
@@ -163,5 +160,30 @@
       </div>
     </section>
   </body>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+  {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+  <script>
+
+  $("#login").on('submit', function(e){
+    isLoading();
+  });
+
+  $("#login-button").on("click", function (e) {
+    isLoading();
+  });
+
+  function isLoading() {
+    $('#login-button').addClass('cursor-not-allowed');
+      $('#login-button').html(`
+          <span class="inline-flex items-center">
+              <svg aria-hidden="true" role="status" class="inline w-5 h-5 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+              </svg> 
+              Logging in...
+          </span>
+      `);
+  }
+  </script>
 </html>
