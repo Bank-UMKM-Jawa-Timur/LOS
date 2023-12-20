830a1a545a59b2bc69b5ed87d3dbbe3ee4b5a902
oong <37205723+oong26@users.noreply.github.com>
2023-12-20 16:07:11 +0700

    feat/dagulir-notification : create notification feature

---
+++ app/Repository/NotificationsRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repository;
+
+use App\Models\Notifications;
+use Illuminate\Support\Facades\DB;
+
+class NotificationsRepository
+{
+    public function list($user_id=null) {
+        $data = Notifications::select('notifications.*', 'u.name', 'u.email', 'u.nip')
+                            ->join('users AS u', 'u.id', 'notifications.user_id')
+                            ->when($user_id, function ($query) use ($user_id) {
+                                $query->where('notifications.user_id', $user_id);
+                            })
+                            ->latest()
+                            ->get();
+
+        return $data;
+    }
+
+    public function store($notification) {
+        DB::table('notifications')->insert($notification);
+    }
+
+    public function read($id) {
+        DB::table('notifications')
+            ->where('id', $id)
+            ->update([
+                'is_read' => 1,
+                'updated_at' => date('Y-m-d H:i:s')
+            ]);
+    }
+
+    public function delete($id) {
+        DB::table('notifications')->delete($id);
+    }
+}
