f08e283e1c8f7c1b0267d3ddec964ae38db97076
oong <37205723+oong26@users.noreply.github.com>
2023-12-20 16:10:55 +0700

    feat/dagulir-notification : list & get limit notification

--- app/Repository/NotificationsRepository.php
+++ app/Repository/NotificationsRepository.php
@@ -7,14 +7,27 @@ use Illuminate\Support\Facades\DB;
 
 class NotificationsRepository
 {
-    public function list($user_id=null) {
+    public function list($user_id=null, $limit=10) {
         $data = Notifications::select('notifications.*', 'u.name', 'u.email', 'u.nip')
                             ->join('users AS u', 'u.id', 'notifications.user_id')
                             ->when($user_id, function ($query) use ($user_id) {
                                 $query->where('notifications.user_id', $user_id);
                             })
                             ->latest()
-                            ->get();
+                            ->paginate($limit);
+
+        return $data;
+    }
+
+    public function getWithLimit($user_id=null, $limit=5) {
+        $data = Notifications::select('notifications.*', 'u.name', 'u.email', 'u.nip')
+                                ->join('users AS u', 'u.id', 'notifications.user_id')
+                                ->when($user_id, function ($query) use ($user_id) {
+                                    $query->where('notifications.user_id', $user_id);
+                                })
+                                ->latest()
+                                ->limit($limit)
+                                ->get();
 
         return $data;
     }
