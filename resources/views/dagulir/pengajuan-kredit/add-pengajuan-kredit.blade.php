f50a6659e6bcf8543a3de5dff18cc7a4b845ae6e
oong <37205723+oong26@users.noreply.github.com>
2023-12-20 12:28:57 +0700

    develop-dagulir : fix kecamatan ktp onchange

--- resources/views/dagulir/pengajuan-kredit/add-pengajuan-kredit.blade.php
+++ resources/views/dagulir/pengajuan-kredit/add-pengajuan-kredit.blade.php
@@ -891,6 +891,31 @@ $dataIndex = match ($skema) {
                 $("#kecamatan").empty();
             }
         });
+        $('#kecamatan').change(function() {
+            var kecID = $(this).val();
+            if (kecID) {
+                $.ajax({
+                    type: "GET",
+                    url: "{{ route('getDesa') }}?kecID=" + kecID,
+                    dataType: 'JSON',
+                    success: function(res) {
+                        if (res) {
+                            $("#desa").empty();
+                            $("#desa").append('<option value="0">---Pilih Desa---</option>');
+                            $.each(res, function(nama, kode) {
+                                $('#desa').append(`
+                                    <option value="${kode}">${nama}</option>
+                                `);
+                            });
+                        } else {
+                            $("#desa").empty();
+                        }
+                    }
+                });
+            } else {
+                $("#desa").empty();
+            }
+        });
         $('#kabupaten_domisili').change(function() {
             var kabID = $(this).val();
             if (kabID) {
