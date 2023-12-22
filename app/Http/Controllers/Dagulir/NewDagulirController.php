e5784d185d8d6845c486c89625fe3f44438413a4
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2023-12-22 14:46:45 +0700

    fix/review-dagulir: List Pengajuan dimunculkan tiap role

--- app/Http/Controllers/Dagulir/NewDagulirController.php
+++ app/Http/Controllers/Dagulir/NewDagulirController.php
@@ -1598,9 +1598,10 @@ class NewDagulirController extends Controller
             $pengajuan_dagulir = $this->repo->get($search,$limit,$page, 'Pincab', $id_user,$allFilter);
             $pengajuan_sipde = $this->repo->get($search,$limit,$page, 'Staf Analis Kredit', $id_user, $allFilter, 'sipde');
         } else {
-            $pengajuan_dagulir = $this->repo->get($search,$limit,$page, 'Staf Analis Kredit', $id_user,$allFilter);
-            $pengajuan_sipde = $this->repo->get($search,$limit,$page, 'Staf Analis Kredit', $id_user, $allFilter, 'sipde');
+            $pengajuan_dagulir = $this->repo->get($search,$limit,$page, 'Administrator', $id_user,$allFilter);
+            $pengajuan_sipde = $this->repo->get($search,$limit,$page, 'Administrator', $id_user, $allFilter, 'sipde');
         }
+
         return view('dagulir.index',[
             'data' => $pengajuan_dagulir,
             'data_sipde' => $pengajuan_sipde,
