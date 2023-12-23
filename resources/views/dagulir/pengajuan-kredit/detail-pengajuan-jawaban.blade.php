c4c40c640cced9d6170b91313a31de82d7fa4212
Oktaviand <108319520+biasalaa@users.noreply.github.com>
2023-12-23 16:25:24 +0700

    Feat/modul loading (#73)
    * feat/modul-loading:fix action href on index dagulir
    
    * feat/modul-loading:add loading to review
    
    * feat/modul-loading: add loading review and aprroval pincab
    
    * feat/modul-loading: fix status on cetak surat
    
    * feat/modul-loading:add loading for upload sppk and pk
    
    * feat/modul-loading:add loading before click edit
    
    * feat/modul-loading:fix href action on index draft
    
    * feat/modul-loading:fix action href on master user
    
    * feat/modul-loading:fix href on list pengajuan dagulir
    
    ---------
    
    Co-authored-by: Rifjan Jundila <62319842+rifjan29@users.noreply.github.com>

--- resources/views/dagulir/pengajuan-kredit/detail-pengajuan-jawaban.blade.php
+++ resources/views/dagulir/pengajuan-kredit/detail-pengajuan-jawaban.blade.php
@@ -1,3 +1,4 @@
+@include('dagulir.pengajuan-kredit.modal.loading-post-review-penyelia')
 @extends('layouts.tailwind-template')
 
 @section('modal')
@@ -750,7 +751,7 @@
                                                             {{ $item->opsi_jawaban == 'file' ? 'col-span-1 order-2' : 'col-span-1 order-3' }}
                                                             {{ $item->nama === "Kebutuhan Kredit" ||
                                                                 $item->nama === "Persentase Net Income" ||
-                                                                $item->nama === "Installment" || $item->nama === "Repayment Capacity" ? 
+                                                                $item->nama === "Installment" || $item->nama === "Repayment Capacity" ?
                                                                 'col-span-2 order-3' : '' }}
                                                             {{ $item->nama === "Jumlah Orang yang menjalankan usaha" ?  'col-span-1 order-3' : '' }}">
                                                         <div class="col-md-12 space-y-4 order">
@@ -2168,6 +2169,9 @@
                             })
                             e.preventDefault()
                         }
+                        else{
+                            $("#preload-data-review-penyelia").removeClass("hidden");
+                        }
                     }
                 }
             }
@@ -2196,6 +2200,10 @@
                         })
                         e.preventDefault()
                     }
+                    else
+                    {
+                        $("#preload-data-review-penyelia").removeClass("hidden");
+                    }
                 }
             }
             else if (role == 'PBP') {
@@ -2220,6 +2228,9 @@
                         })
                         e.preventDefault()
                     }
+                    else{
+                            $("#preload-data-review-penyelia").removeClass("hidden");
+                        }
                 }
             }
             else {
@@ -2379,6 +2390,10 @@
                 text: "Field Pendapat dan usulan harus diisi"
             })
             e.preventDefault()
+        }
+        else
+        {
+            $("#preload-data-review-penyelia").removeClass("hidden");
         }
     })
     // End Validation
