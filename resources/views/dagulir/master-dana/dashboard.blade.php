fa4a033765a0d4761609a2c5dcc44daaf5727118
sahrullahh <49519441+sahrullahh@users.noreply.github.com>
2023-12-27 15:53:08 +0700

    fix/review-dagulir: add pengajuan card in dana dashboard

--- resources/views/dagulir/master-dana/dashboard.blade.php
+++ resources/views/dagulir/master-dana/dashboard.blade.php
@@ -10,20 +10,19 @@
         </h2>
     </div>
 <div class="body-pages space-y-5">
-    <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 justify-center w-full">
+        <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4 lg:gap-2 justify-center mt-5">
         <div class="card p-5 w-full border bg-white h-[127px]">
             <div class="flex gap-5">
                 <div>
-                    <button class="w-20 h-20 p-5 rounded-full bg-red-100">
-                        <iconify-icon icon="healthicons:money-bag-outline" class="text-4xl mt-1 text-red-600"></iconify-icon>
+                    <button class="w-20 h-20 p-5 rounded-full bg-[#9334EA]/20">
+                        <iconify-icon icon="pajamas:chart" class="text-3xl mt-1 text-[#9334EA]"></iconify-icon>
                     </button>
                 </div>
                 <div class="mt-3">
-                    <h2 id="total-modal" class="text-theme-text text-2xl font-bold tracking-tighter">
-                        {{ "Rp.". number_format($total_modal ,2,',','.') }}
-                    </h2>
+                    <h2 class="text-theme-text text-3xl font-bold tracking-tighter">
+                        30                    </h2>
                     <p class="text-gray-500 text-sm tracking-tighter">
-                        Total Modal
+                        Total Pengajuan
                     </p>
                 </div>
             </div>
@@ -31,16 +30,17 @@
         <div class="card p-5 w-full border bg-white h-[127px]">
             <div class="flex gap-5">
                 <div>
-                    <button class="w-20 h-20 p-5 rounded-full bg-yellow-100">
-                        <iconify-icon icon="mdi:cash-clock" class="text-3xl mt-1 text-yellow-600"></iconify-icon>
+                    <button class="w-20 h-20 p-5 rounded-full bg-[#39B568]/20">
+                        <iconify-icon icon="icon-park-outline:check-one" class="text-3xl mt-1 text-[#39B568]">
+                        </iconify-icon>
                     </button>
                 </div>
                 <div class="mt-3">
-                    <h2 id="total-idle" class="text-theme-text text-2xl font-bold tracking-tighter">
-                        {{ "Rp.". number_format($total_idle ,2,',','.') }}
+                    <h2 class="text-theme-text text-3xl font-bold tracking-tighter">
+                        30}
                     </h2>
                     <p class="text-gray-500 text-sm tracking-tighter">
-                        Total Idle
+                        Disetujui
                     </p>
                 </div>
             </div>
@@ -48,44 +48,113 @@
         <div class="card p-5 w-full border bg-white h-[127px]">
             <div class="flex gap-5">
                 <div>
-                    <button class="w-20 h-20 p-5 rounded-full bg-green-100">
-                        <iconify-icon icon="mdi:cash-clock" class="text-3xl mt-1 text-green-600"></iconify-icon>
+                    <button class="w-20 h-20 p-5 rounded-full bg-[#DC3545]/20">
+                        <iconify-icon icon="uil:file-times-alt" class="text-3xl mt-1 text-[#DC3545]"></iconify-icon>
                     </button>
                 </div>
                 <div class="mt-3">
-                    <h2 id="total-idle" class="text-theme-text text-2xl font-bold tracking-tighter">
-                        Rp.0,00
+                    <h2 class="text-theme-text text-3xl font-bold tracking-tighter">
+                        30
                     </h2>
-                    <p class="text-gray-500 text-sm tracking-tighter">
-                        Total Akumulatif
-                    </p>
+                    <p class="text-gray-500 text-sm tracking-tighter">Ditolak</p>
                 </div>
             </div>
         </div>
         <div class="card p-5 w-full border bg-white h-[127px]">
             <div class="flex gap-5">
                 <div>
-                    <button class="w-20 h-20 p-5 rounded-full bg-purple-100">
-                        <iconify-icon icon="solar:wallet-money-linear" class="text-3xl mt-1 text-purple-600"></iconify-icon>
+                    <button class="w-20 h-20 p-5 rounded-full bg-[#E8A525]/20">
+                        <iconify-icon icon="ps:clock" class="text-3xl mt-1 text-[#E8A525]"></iconify-icon>
                     </button>
                 </div>
                 <div class="mt-3">
-                    <h2 id="total-baket" class="text-theme-text text-2xl font-bold tracking-tighter">
-                        {{ "Rp.". number_format($total_baket ,2,',','.') }}
+                    <h2 class="text-theme-text text-3xl font-bold tracking-tighter">
+                        30
                     </h2>
-                    <p class="text-gray-500 text-sm tracking-tighter">
-                        Total Baki Debet
-                    </p>
+                    <p class="text-gray-500 text-sm tracking-tighter">Diproses</p>
                 </div>
             </div>
         </div>
     </div>
-    <div class="bg-white border w-full p-3 rounded">
-        <div class="head p-2">
-            <h2 class="font-bold tracking-tighter">Pelimpahan Dana Masing-Masing Cabang</h2>
+    <div class="lg:flex grid grid-cols-1 gap-5">
+        <div class="grid lg:grid-cols-1 md:grid-cols-2 grid-cols-1 gap-5 justify-center w-full">
+            <div class="card p-5 w-full border bg-white h-[127px]">
+                <div class="flex gap-5">
+                    <div>
+                        <button class="w-20 h-20 p-5 rounded-full bg-red-100">
+                            <iconify-icon icon="healthicons:money-bag-outline" class="text-4xl mt-1 text-red-600"></iconify-icon>
+                        </button>
+                    </div>
+                    <div class="mt-3">
+                        <h2 id="total-modal" class="text-theme-text text-2xl font-bold tracking-tighter">
+                            {{ "Rp.". number_format($total_modal ,2,',','.') }}
+                        </h2>
+                        <p class="text-gray-500 text-sm tracking-tighter">
+                            Total Modal
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="card p-5 w-full border bg-white h-[127px]">
+                <div class="flex gap-5">
+                    <div>
+                        <button class="w-20 h-20 p-5 rounded-full bg-yellow-100">
+                            <iconify-icon icon="mdi:cash-clock" class="text-3xl mt-1 text-yellow-600"></iconify-icon>
+                        </button>
+                    </div>
+                    <div class="mt-3">
+                        <h2 id="total-idle" class="text-theme-text text-2xl font-bold tracking-tighter">
+                            {{ "Rp.". number_format($total_idle ,2,',','.') }}
+                        </h2>
+                        <p class="text-gray-500 text-sm tracking-tighter">
+                            Total Idle
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="card p-5 w-full border bg-white h-[127px]">
+                <div class="flex gap-5">
+                    <div>
+                        <button class="w-20 h-20 p-5 rounded-full bg-green-100">
+                            <iconify-icon icon="mdi:cash-clock" class="text-3xl mt-1 text-green-600"></iconify-icon>
+                        </button>
+                    </div>
+                    <div class="mt-3">
+                        <h2 id="total-idle" class="text-theme-text text-2xl font-bold tracking-tighter">
+                            Rp.0,00
+                        </h2>
+                        <p class="text-gray-500 text-sm tracking-tighter">
+                            Total Akumulatif
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="card p-5 w-full border bg-white h-[127px]">
+                <div class="flex gap-5">
+                    <div>
+                        <button class="w-20 h-20 p-5 rounded-full bg-purple-100">
+                            <iconify-icon icon="solar:wallet-money-linear" class="text-3xl mt-1 text-purple-600"></iconify-icon>
+                        </button>
+                    </div>
+                    <div class="mt-3">
+                        <h2 id="total-baket" class="text-theme-text text-2xl font-bold tracking-tighter">
+                            {{ "Rp.". number_format($total_baket ,2,',','.') }}
+                        </h2>
+                        <p class="text-gray-500 text-sm tracking-tighter">
+                            Total Baki Debet
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div id="total-dana"></div>
+        <div class="bg-white border w-full p-3 rounded">
+            <div class="head p-2">
+                <h2 class="font-bold tracking-tighter">Pelimpahan Dana Masing-Masing Cabang</h2>
+            </div>
+            <div id="total-dana"></div>
+        </div>
     </div>
+
 </div>
 </section>
 
