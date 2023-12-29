e60ab6fd9520150e0b48ab8e88156b6591d361c7
29rifjanj <rjndla@gmail.com>
2023-12-30 04:36:56 +0700

    fix: sidebar

--- resources/views/components/new/sidebar.blade.php
+++ resources/views/components/new/sidebar.blade.php
@@ -228,23 +228,7 @@
                             </button>
                         </a>
                     </li>
-                    <li class="menu-item">
-                        <a href="{{ url('pengajuan-kredit') }}">
-                            <button class="item-link @active('pengajuan-kredit,pengajuan-kredit.*', 'active-menu')  w-full flex gap-2 font-medium text-left px-4 py-3 rounded-md">
-                                <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24">
-                                        <g fill="none" stroke="currentColor" stroke-width="1.5">
-                                            <path
-                                                d="M2 12c0-3.771 0-5.657 1.172-6.828C4.343 4 6.229 4 10 4h4c3.771 0 5.657 0 6.828 1.172C22 6.343 22 8.229 22 12c0 3.771 0 5.657-1.172 6.828C19.657 20 17.771 20 14 20h-4c-3.771 0-5.657 0-6.828-1.172C2 17.657 2 15.771 2 12Z" />
-                                            <path stroke-linecap="round" d="M10 16H6m8 0h-1.5M2 10h20" opacity=".5" />
-                                        </g>
-                                    </svg>
-                                </span>
-                                <span class="ml-2 mt-[2px]"> Analisa kredit</span>
-                            </button>
-                        </a>
-                    </li>
+
                     <div class="dropdown-menu-link hidden">
                         <ul class="space-y-1 p-2 mt-3 bg-gray-50">
                             <li>
