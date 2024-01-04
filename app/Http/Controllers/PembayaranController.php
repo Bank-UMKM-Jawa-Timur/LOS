eb563581c0192ecc644daa2e2a2e002a7b21f562
29rifjanj <rjndla@gmail.com>
2024-01-04 15:12:24 +0700

    fix: pembayaran progress

--- app/Http/Controllers/PembayaranController.php
+++ app/Http/Controllers/PembayaranController.php
@@ -5,11 +5,40 @@ namespace App\Http\Controllers;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
+use Pion\Laravel\ChunkUpload\Handler\HandlerFactory;
+use Pion\Laravel\ChunkUpload\Receiver\FileReceiver;
 
 class PembayaranController extends Controller
 {
     function index() {
         return view('pembayaran.upload',['data' => null]);
+    }
+
+    function upload(Request $request) {
+
+
+        // Process file_txt
+        if ($request->hasFile('file_txt')) {
+            $file = $request->file('file_txt');
+            $filename_txt = 'file'.'.'.$file->extension();
+            $file->storeAs('file',$filename_txt);
+        }
+        // Process file_dic
+        if ($request->has('file_dic')) {
+            $file = $request->file('file_dic');
+            $filename = 'dictionary'.'.'.$file->extension();
+            $file->storeAs('dictionary/',$filename);
+        }
+        //start file_nomi
+        if ($request->has('file_nomi')) {
+            $file_nomi = $request->file('file_nomi');
+            $filename = 'nomi'.'.'.$file_nomi->extension();
+            $file_nomi->storeAs('file-nomi/',$filename);
+        }
+
+        return response()->json(['success' => true]);
+
+
     }
 
     function store(Request $request) {
@@ -28,11 +57,6 @@ class PembayaranController extends Controller
 
             // Process file_txt
             $filename_txt = 'file.txt';
-            if ($request->hasFile('file_txt')) {
-                $file = $request->file('file_txt');
-                $filename_txt = 'file'.'.'.$file->extension();
-                $file->storeAs('file',$filename_txt);
-            }
             $fp = fopen(storage_path('app/file/'.$filename_txt), "r");
             $txt_data = [];
             while(!feof($fp)){
@@ -42,20 +66,11 @@ class PembayaranController extends Controller
                 }
             }
             // Process file_dic
-            if ($request->has('file_dic')) {
-                $file = $request->file('file_dic');
-                $filename = 'dictionary'.'.'.$file->extension();
-                $file->storeAs('dictionary/',$filename);
-            }
             $theArray = Excel::toCollection([], storage_path('app/dictionary/').'dictionary.xlsx');
             // end process file_dic
 
             //start file_nomi
-            if ($request->has('file_nomi')) {
-                $file_nomi = $request->file('file_nomi');
-                $filename = 'nomi'.'.'.$file_nomi->extension();
-                $file_nomi->storeAs('file-nomi/',$filename);
-            }
+            $filename = 'nomi.xlsx';
             $theArrayNomi = Excel::toCollection([],storage_path('app/file-nomi/').$filename);
             for ($i=0; $i < count($theArrayNomi[0]); $i++) {
                 if ($i > 0) {
@@ -136,64 +151,7 @@ class PembayaranController extends Controller
         // }
     }
 
-    function filter(Request $request) {
-        $fp = fopen(storage_path('app/file/'.'file.txt'), "r");
-
-        $txt_data = [];
-        while(!feof($fp)){
-            $line = fgets($fp);
-            if (!str_contains($line, "")) {
-                array_push($txt_data, utf8_encode($line));
-            }
-        }
-        // Process file_dic
-        $fieldPositions = [[
-            'field' => 'HLSTAT',
-            'from' => 1,
-            'to' => 1],];
-
-        $theArray = Excel::toCollection([], storage_path('app/dictionary/').'dictionary.xlsx');
-        for ($i=0; $i < count($theArray[0]); $i++) {
-            // Sequence(HLSEQN) | No. Loan(HLLNNO) | Tanggal Pembayaran(HLDTVL) | Nominal(HLOPMT)
-            if ($i > 4) {
-                array_push($fieldPositions,[
-                    'field' => $theArray[0][$i][0],
-                    'from' => $theArray[0][$i][1],
-                    'to' => $theArray[0][$i][2],
-                ]);
-            }
-
-        }
-
-        // $body = [
-        //     'file_json' => $txt_data,
-        //     'dictionary' => $fieldPositions
-        // ];
-        // $pembayaran = Http::post(env('EXTRA_HOST').'/pembayaran', $body)->json();
-        // $result = $pembayaran['data'];
-        $result = array();
-        for ($i = 0; $i < count($txt_data); $i++) {
-            $val = trim($txt_data[$i]);
-            $objects = array();
-            foreach ($fieldPositions as $j) {
-                $value = substr($val, $j['from']-1, $j['to']-$j['from']+1);
-                $value = ltrim(rtrim($value));
-                $objects[$j['field']] = $value;
-            }
-            $result[] = $objects;
-        }
-
-        foreach ($result as $key => $value) {
-            if ($request->get('filter') != '0') {
-                if ($value['HLACKY'] != $request->get('filter')) {
-                    unset($result[$key]);
-                }
-            }
-
-        }
-        return view('pembayaran.upload',['data' => $result]);
-        // return view('pembayaran.upload',['data' => $pembayaran['data']]);
-
+    function proses_data() {
 
     }
 }
--- resources/views/pembayaran/upload.blade.php
+++ resources/views/pembayaran/upload.blade.php
@@ -1,4 +1,49 @@
 @extends('layouts.tailwind-template')
+@push('script-inject')
+    {{-- <script src="https://cdn.jsdelivr.net/npm/resumablejs@1.1.0/resumable.min.js"></script> --}}
+    <script>
+        $(document).ready(function () {
+            $('#checkFile').hide();
+            $('#fileUploadForm').submit(function (e) {
+                e.preventDefault();
+
+                var form = $(this);
+                var formData = new FormData(form[0]);
+                var progressBarContainer = $('#progressBarContainer');
+                var progressBar = $('.progress-bar');
+
+                progressBarContainer.show(); // Show progress bar container
+                progressBar.css('width', '0%'); // Reset progress bar width
+
+                $.ajax({
+                    xhr: function () {
+                        var xhr = new window.XMLHttpRequest();
+
+                        xhr.upload.addEventListener("progress", function (evt) {
+                            if (evt.lengthComputable) {
+                                var percentComplete = (evt.loaded / evt.total) * 100;
+                                progressBar.css('width', percentComplete + '%');
+                                progressBar.html(percentComplete + '%');
+                            }
+                        }, false);
+
+                        return xhr;
+                    },
+                    type: form.attr('method'),
+                    url: form.attr('action'),
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    success: function () {
+                        console.log('File has uploaded');
+                        $('#uploadButton').hide();
+                        $('#checkFile').show();
+                    }
+                });
+            });
+        });
+    </script>
+@endpush
 @section('content')
 <section class="p-5 overflow-y-auto mt-5">
     <div class="head space-y-5 w-full font-poppins">
@@ -13,12 +58,13 @@
     </div>
     <div class="body-pages">
         <div class="table-wrapper border bg-white mt-3 p-5">
-            <form action="{{ route('pembayaran.store') }}" enctype="multipart/form-data" method="POST">
+
+            <form id="fileUploadForm" action="{{ route('pembayaran.upload') }}" enctype="multipart/form-data" method="POST">
                 @csrf
                 <div class="form-group-3">
                     <div class="input-box">
                         <label for="">File Txt</label>
-                        <input type="file" name="file_txt" class="form-input" id="">
+                        <input type="file" name="file_txt" class="form-input" id="browseFile">
                     </div>
                     <div class="input-box">
                         <label for="">Dictionary</label>
@@ -29,8 +75,14 @@
                         <input type="file" name="file_nomi" class="form-input" id="">
                     </div>
                 </div>
+                <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 my-4" id="progressBarContainer" style="display:none;">
+                    {{-- <div class="progress-bar bg-blue-500" style="width: 20%;"></div> --}}
+                    <div class="progress-bar bg-blue-600 h-2.5 rounded-full text-xs leading-none text-center text-white" style="width: 0%"></div>
+                </div>
+
                 <div class="flex justify-end my-5 py-4">
-                    <button class="px-5 py-2 border rounded bg-theme-primary text-white">Check File</button>
+                    <button type="submit" class="px-5 py-2 border rounded bg-theme-primary text-white" id="uploadButton">Upload File</button>
+                    <a href="{{ route('pembayaran.store') }}" class="px-5 py-2 border rounded bg-theme-primary text-white" id="checkFile">Check File</a>
                 </div>
             </form>
             @if ($data != null)
