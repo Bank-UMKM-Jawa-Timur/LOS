e5e9925cb1a3278a03f3077e5ff367ec175d2e40
29rifjanj <rjndla@gmail.com>
2024-02-25 22:59:19 +0700

    feat: detail pengajuan

--- resources/views/dagulir/pengajuan/edit-dagulir-analisis.blade.php
+++ resources/views/dagulir/pengajuan/edit-dagulir-analisis.blade.php
@@ -20,16 +20,16 @@
                     </div>
                 </div>
             </div>
-            <dl class="max-w-md text-gray-900 divide-y bg-theme-primary/5 dark:text-white dark:divide-gray-700 p-3">
+            <dl class="max-w-full text-gray-900 divide-y bg-theme-primary/5 dark:text-white dark:divide-gray-700 p-3">
                 @foreach ($pendapat as $item)
                 <div class="flex flex-col pb-3 mb-1">
                     <div class="flex gap-3 mt-3">
                         <div class="rounded-full w-4 h-4 border border-theme-primary bg-theme-primary"></div>
-                        <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">{{$item->created_at}}</dt>
+                        <dt class="mb-1 text-gray-500 md:text-sm dark:text-gray-400">{{ Carbon\Carbon::parse($item->created_at)->format('d-m-Y H:i:s') }}</dt>
                     </div>
                     <div class="flex gap-3 mt-3">
                         <div class="rounded-full w-4 h-4 border border-theme-primary bg-theme-primary hidden"></div>
-                        <dd class="text-lg font-semibold ml-5">{{$item->alasan}}</dd>
+                        <dd class="text-lg text-gray-900 font-semibold ml-5">{{$item->alasan}}</dd>
                     </div>
                 </div>
                 @endforeach
